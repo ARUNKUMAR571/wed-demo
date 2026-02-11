955af511f5255b1763c818887b2027d70c64c5c6
arunkumar <arunkumar.sodad571@gmail.com>
2026-02-11 13:24:51 +0530

    added ecr url

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 aws ecr get-login-password --region us-east-1 |
-docker login --username AWS --password-stdin 083347785371.dkr.ecr.us-east-1.amazonaws.com
+docker login --username AWS --password-stdin 83347785371.dkr.ecr.us-east-1.amazonaws.com
 
-docker pull 083347785371.dkr.ecr.us-east-1.amazonaws.com/cicd-web-app:latest
+docker pull 083347785371.dkr.ecr.us-east-1.amazonaws.com/wed-demo-ecr:latest
 
 docker run -d -p 80:80 --name webapp \
-083347785371.dkr.ecr.us-east-1.amazonaws.com/cicd-web-app:latest
+083347785371.dkr.ecr.us-east-1.amazonaws.com/wed-demo-ecr:latest
