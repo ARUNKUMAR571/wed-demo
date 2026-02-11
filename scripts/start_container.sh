430956986a47c0e6c2e24a24e3c3c4ba747acc57
arunkumar <arunkumar.sodad571@gmail.com>
2026-02-11 12:49:57 +0530

    initial commit

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+aws ecr get-login-password --region us-east-1 |
+docker login --username AWS --password-stdin 083347785371.dkr.ecr.us-east-1.amazonaws.com
+
+docker pull 083347785371.dkr.ecr.us-east-1.amazonaws.com/cicd-web-app:latest
+
+docker run -d -p 80:80 --name webapp \
+083347785371.dkr.ecr.us-east-1.amazonaws.com/cicd-web-app:latest
---
+++ scripts/stop_container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker stop webapp || true
+docker rm webapp || true
